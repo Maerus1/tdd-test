8392eee0bfba0ed85c96c5d4f376b16330242d9b
Daniel Dorsey <danieldorsey472@gmail.com>
2019-01-26 13:10:12 -0500

    Add clearing and operation chaining

--- calculator/src/util/Calculator.js
+++ calculator/src/util/Calculator.js
@@ -1,15 +1,47 @@
 export default class Calculator {
   constructor(){
-    
+    this.result = 0;
+    this.operator = null;
+    this.terms = [];
   }
 
   //basic operations
-  add = (x, y) => x + y;
+  add = (x, y) => this.result = x + y;
 
-  subtract = (x, y) => x - y;
+  subtract = (x, y) => this.result = x - y;
 
-  multiply = (x, y) => x * y;
+  multiply = (x, y) => this.result = x * y;
 
-  divide = (x, y) => x / y;
+  divide = (x, y) => this.result = x / y;
+
+  clear = () => this.result = 0;
+
+  evaluate = () => {
+    let operation = [];
+    for(let i in this.terms){
+      
+      if(operation.length < 3){
+        operation.push(this.terms[i]);
+      }
+      if(operation.length === 3){
+        switch(operation[1]){
+          case '+':
+            operation = [this.add(operation[0], operation[2])];
+            break;
+          case '-':
+            operation = [this.subtract(operation[0], operation[2])];
+            break;
+          case '*':
+            operation = [this.multiply(operation[0], operation[2])];
+            break;
+          case '/':
+            operation = [this.divide(operation[0], operation[2])];
+            break;
+        }
+      }
+    }
+
+    return this.result = operation[0];
+  }
 
 }
--- calculator/tests/unit/Calculator.spec.js
+++ calculator/tests/unit/Calculator.spec.js
@@ -3,25 +3,45 @@ import  Calculator  from '@/util/Calculator.js';
 describe('Calcuator.js', () => {
   it('adds two numbers', () => {
     let calculator = new Calculator();
-    let answer = calculator.add(5, 5);
-    expect(answer).toEqual(10);
+    calculator.add(5, 5)
+    expect(calculator.result).toEqual(10);
   });
 
   it('subtracts two numbers', () => {
     let calculator = new Calculator();
-    let answer = calculator.subtract(5, 3);
-    expect(answer).toEqual(2);
+    calculator.subtract(5, 3)
+    expect(calculator.result).toEqual(2);
   });
 
   it('multiplies two numbers', () => {
     let calculator = new Calculator();
-    let answer = calculator.multiply(3, 3);
-    expect(answer).toEqual(9);
+    calculator.multiply(3, 3)
+    expect(calculator.result).toEqual(9);
   });
 
   it('divides two numbers', () => {
     let calculator = new Calculator();
-    let answer = calculator.divide(9, 3);
-    expect(answer).toEqual(3);
+    calculator.divide(9, 3)
+    expect(calculator.result).toEqual(3);
   });
-});
+
+  it('clears the result', () => {
+    let calculator = new Calculator();
+    calculator.add(4, 4);
+    calculator.clear();
+    expect(calculator.result).toEqual(0);
+  });
+
+  it('has operation variables', () => {
+    let calculator = new Calculator();
+    expect(calculator.operator).toBeNull();
+    expect(calculator.terms).toBeDefined();
+  });
+
+  it('evaluates all terms', () => {
+    let calculator = new Calculator();
+    calculator.terms.push(4, '+', 6, '/', 5, '*', 6);
+    calculator.evaluate();
+    expect(calculator.result).toEqual(12);
+  });
+}); 
